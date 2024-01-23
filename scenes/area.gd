8261be5b1b3ef18f0672e49ebe0b278dade007d7
Raul Gabrich <raulgbrmf@gmail.com>
2024-01-22 21:18:24 -0300

    Add box movement scheme.

---
+++ scenes/area.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal custom_input_event
+
+var direction:Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_input_event(viewport, event, shape_idx):
+	custom_input_event.emit(self,event)
---
+++ scenes/box.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+var SPEED = 1000
+var moving:bool = false
+var target_direction = Vector2.ZERO
+
+
+func _ready():
+	#Conects the signs of input_event of each Area2d
+	$AreaUp.direction =  Vector2.UP
+	$AreaLeft.direction =  Vector2.LEFT
+	$AreaRight.direction =  Vector2.RIGHT
+	$AreaDown.direction =  Vector2.DOWN
+
+	# Agora você conecta sem passar a direção como argumento
+	$AreaLeft.connect("custom_input_event", Callable(self, "_on_Area_input_event"))
+	$AreaUp.connect("custom_input_event", Callable(self, "_on_Area_input_event"))
+	$AreaRight.connect("custom_input_event", Callable(self, "_on_Area_input_event"))
+	$AreaDown.connect("custom_input_event", Callable(self, "_on_Area_input_event"))
+	
+
+func _process(delta):
+	if moving:
+		_move_step(delta)
+
+
+func _on_Area_input_event(area,event):
+	if event is InputEventMouseButton and event.pressed:
+		_move_box(area.direction)
+
+	
+func _move_box(direction):
+	if not moving:
+		moving = true
+		target_direction = direction
+
+func _move_step(delta):
+	var movement = target_direction * SPEED * delta
+	position += movement
+
+func stop():
+	pass
+
